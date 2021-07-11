ff405b75febf14c15efeb5ac74438c4a6c181c49
V Lazarenko <vlazarenko@ptsecurity.com>
2021-07-11 17:35:53 +0300

    template

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
-	})
-	http.ListenAndServe(":80", nil)
+
+	http.HandleFunc("/", HelloHandler)
+	fmt.Println("Server started at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, there\n")
 }
